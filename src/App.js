59cee6ba0602799a51949c1d23b52794a1e387c4
Aditya <akaditya394@gmail.com>
2022-02-14 23:47:04 +0530

    api call functionality added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,15 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        setMovies(data.results);
+        const transformedMovies = movies.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
       });
   };
 
