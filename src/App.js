aefb0aa68c574262e6e2ab2d619203f869f95dbe
Aditya <akaditya394@gmail.com>
2022-02-14 23:48:50 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        const transformedMovies = movies.map((movieData) => {
+        const transformedMovies = data.movies.map((movieData) => {
           return {
             id: movieData.episode_id,
             title: movieData.title,
