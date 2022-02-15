bff837399e2836f68f7c7f29c38663f503fb3b7c
Aditya <akaditya394@gmail.com>
2022-02-15 23:19:49 +0530

    error message set

--- src/App.js
+++ src/App.js
@@ -11,19 +11,29 @@ function App() {
   async function fetchMoviesHandler() {
     setLoading(true);
     setError(null);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setLoading(false);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/films/");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
   let content = <p>Found no movies</p>;
